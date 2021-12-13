bba10b8bd6b9d0bf044cf6bea52655c414f7e1de
Libby <libby_dunham@yahoo.com>
2021-12-12 19:11:34 -0500

    work pwease

---
+++ Assets/UI/ItemPoints.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPoints : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(transform.name == "ThirdPersonController")
+        {
+            KeepScore.Score += 1;
+            Debug.Log("Item Collected");
+        }
+    }
+}
